87d86b74f0e60e3260bdca3be92b5d2d861ef815
G00DCOD3R <cichockitymon@gmail.com>
2023-01-25 21:28:03 +0100

    comments to track.js

--- SignIn/track.js
+++ SignIn/track.js
@@ -1,19 +1,30 @@
-let mouseClicks = 0; 
-let keysPressed = 0;
-let charsTyped = 0;
-let startTime = new Date();
-// document.addEventListener('onmousedown', trackMouse());
+let mouseClicks = 0; // number of mouse clicks
+let keysPressed = 0; // number of keys that was pressed (such as 'a', 'backspace', etc.)
+let charsTyped = 0; // number of characters that was pressed (letters A - Z)
+
+let startTime = new Date(); // time when user entered page 
+
+// listener for keys 
+// tracks every key, only some keys represent characters
 document.addEventListener('keydown', (event) => {
+
+    // where changes in document has to be made
     let element = document.getElementById("keysPressed");
-    element.innerHTML = '<p id = "keysPressed">Keys pressed = ' + keysPressed + '</p>';
     let element2 = document.getElementById("charsTyped");
+
+    // make changes 
+    element.innerHTML = '<p id = "keysPressed">Keys pressed = ' + keysPressed + '</p>';
     element2.innerHTML = '<p id = "charsTyped">Chars typed = ' + charsTyped + '</p>';
+
+    // update counters 
     console.log(event.keyCode);
     if(event.keyCode >= 65 && event.keyCode <= 90) {
         charsTyped++;
     }
     keysPressed++;
 });
+
+// listener for tracking mouse clicks 
 document.addEventListener('mousedown', (event) => {
     let element = document.getElementById("mouseClicks");
     element.innerHTML = '<p id = "mouseClicks"> Mouse clicks = ' + mouseClicks + ' </p>';
@@ -21,6 +32,7 @@ document.addEventListener('mousedown', (event) => {
     mouseClicks++;
 })
 
+// updating time every 100ms. Body event 'onload' calls this function every 100ms
 function getTime() {
     let element = document.getElementById("timeSpent");
     let time = new Date() - startTime; 
